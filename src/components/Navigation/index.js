3c912b0ac7809b3674804309b3a48f7279bb8309
Mikko Maja <mikmaj88@gmail.com>
2019-02-04 14:00:49 +0200

    Initial commit

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Navigation.scss'
+
+const Navigation = ({ links }) => {
+    return (
+        <div className="Navigation">
+            <ul>
+                {links.map(link => (
+                    <li key={link.to}>
+                        <a href={link.to}>{link.label}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Navigation;
