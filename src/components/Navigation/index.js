fd506576ea0061b3b6061b06dd22b5a66cfaadb8
Mikko Maja <mikmaj88@gmail.com>
2019-03-26 16:40:00 +0200

    Add user authentication to session

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,33 +5,39 @@ import SignOutButton from '../SignOut'
 import * as ROUTES from '../../constants/routes'
 import './Navigation.scss'
 
+const Navigation = ({ authUser }) => (
+    <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+)
 
-const Navigation = () => {
-    return (
-        <div>
-            <ul>
-                {/* Get navigation routes from constants/routes.js */}
-                <li>
-                    <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-                </li>
-                <li>
-                    <Link to={ROUTES.LANDING}>Landing</Link>
-                </li>
-                <li>
-                    <Link to={ROUTES.HOME}>Home</Link>
-                </li>
-                <li>
-                    <Link to={ROUTES.ACCOUNT}>Account</Link>
-                </li>
-                <li>
-                    <Link to={ROUTES.ADMIN}>Admin</Link>
-                </li>
-                <li>
-                    <SignOutButton />
-                </li>
-            </ul>
-        </div>
-    );
-};
+// Links to show if a user is authenticated
+const NavigationAuth = () => (
+    <ul>
+        {/* Get navigation routes from constants/routes.js */}
+        <li>
+            <Link to={ROUTES.LANDING}>Landing</Link>
+        </li>
+        <li>
+            <Link to={ROUTES.HOME}>Home</Link>
+        </li>
+        <li>
+            <Link to={ROUTES.ACCOUNT}>Account</Link>
+        </li>
+        <li>
+            <SignOutButton />
+        </li>
+    </ul>
+)
+
+// Links to show if a user is not authenticated
+const NavigationNonAuth = () => (
+    <ul>
+        <li>
+            <Link to={ROUTES.LANDING}>Landing</Link>
+        </li>
+        <li>
+            <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+        </li>
+    </ul>
+)
 
 export default Navigation;
